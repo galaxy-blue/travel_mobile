52c4608a04f59a6eea7531931af3510e2dc37a78
tl <2465416251@qq.com>
2019-06-15 00:12:29 +0800

    swiper

--- src/main.js
+++ src/main.js
@@ -4,12 +4,15 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import fastClick from 'fastclick'    //解决移动端300ms点击延迟问题
+import MintUI from 'mint-ui'
+import 'mint-ui/lib/style.css'
 import './assets/styles/reset.css'    //重置css
 import './assets/styles/border.css'  //移动端1px边框问题
 
 
 Vue.config.productionTip = false
 fastClick.attach(document.body)
+Vue.use(MintUI)
 
 /* eslint-disable no-new */
 new Vue({
---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="home_swiper">
+    <mt-swipe :auto="4000">
+      <mt-swipe-item v-for="list in swiperList" :key="list.id">
+        <img :src="list.item" alt="">
+      </mt-swipe-item>
+    </mt-swipe>
+  </div>
+</template>
+
+<script>
+import swiper01 from "@/assets/images/swiper01.png";
+import swiper02 from "@/assets/images/swiper02.png";
+import swiper03 from "@/assets/images/swiper03.png";
+export default {
+  name: "swiper",
+  data() {
+    return {
+      swiperList: [
+        {
+          id: "01",
+          item: swiper01
+        },
+        {
+          id: "02",
+          item: swiper02
+        },
+        {
+          id: "03",
+          item: swiper03
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<style scoped>
+.home_swiper {
+  width: 100%;
+  height: 2rem;
+}
+.home_swiper img {
+  height: 100%;
+  width: 100%;
+}
+</style>
